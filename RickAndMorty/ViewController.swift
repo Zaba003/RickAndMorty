32596e45fb1d47f43b86e65ea3e93bac9d3ed15a
Kirill Zaborsky <avto.oil@gmail.com>
2020-07-07 20:58:35 +0700

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Кирилл Заборский on 07.07.2020.
+//  Copyright © 2020 Кирилл Заборский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
